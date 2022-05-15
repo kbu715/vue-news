ee29d7962fcd38f10d5794c0207ebb5068c8c6a1
kbu715 <kbu715@gmail.com>
2022-05-15 17:07:55 +0900

    feat: implement API using axios

---
+++ MEMO.md
@@ -0,0 +1,5 @@
+# Vue Fundamental
+
+## Vue Life cycle
+
+[https://beomy.tistory.com/47](https://beomy.tistory.com/47)
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    ask
+    <div v-for="item in asks" :key="item.id">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
+import { fetchAskList } from '../api/index.js';
 export default {
-
+  data() {
+    return {
+      asks: []
+    }
+  },
+  created() {
+    fetchAskList()
+          .then(response => this.asks = response.data)
+          .catch(err => console.log(err));
+  }
 }
 </script>
 
