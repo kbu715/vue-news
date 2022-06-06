e9534f3bde749d1c4ae25a41a1dfc2ed9ef08db0
kbu715 <kbu715@gmail.com>
2022-06-06 23:54:45 +0900

    feat: 라우터 네비게이션 가드 beforeEnter와 mixins를 이용 ux 개선

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue'
 import ItemView from '../views/ItemView.vue'
-import createListView from '../components/CreateListView'
+import bus from '../utils/bus'
+import { store } from '../store/index'
 
 Vue.use(VueRouter);
 
@@ -14,21 +18,43 @@ export const router = new VueRouter({
       redirect: '/news',
     },
     {
-      // path: url주소
       path: '/news',
       name: 'news',
-      // component: url 주소로 갔을 때 표시될 컴포넌트
-      component: createListView('NewsView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+        .then(() => next())
+        .catch((error) => {
+          console.log(error);
+        })
+      },
+      component: NewsView,
     },
     {
       path: '/ask',
       name: 'ask',
-      component: createListView('AskView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+        .then(() => next())
+        .catch((error) => {
+          console.log(error);
+        })
+      },
+      component: AskView,
     },
     {
       path: '/jobs',
       name: 'jobs',
-      component: createListView('JobsView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+        .then(() => next())
+        .catch((error) => {
+          console.log(error);
+        })
+      },
+      component: JobsView,
     },
     {
       path: '/user/:id',
---
+++ src/views/NewsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ListItem />
+  </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+import ListMixin from '../mixins/ListMixin.js';
+export default {
+  components: {
+    ListItem,
+  },
+  mixins: [ListMixin],
+};
+</script>
