91d3b498cd4944313f7030d30dbd3fc8bdea4a0c
kbu715 <kbu715@gmail.com>
2022-06-06 23:55:28 +0900

    feat: promise -> async/await

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,61 +1,20 @@
 import { fetchCommentItem, fetchUserInfo, fetchList } from '../api';
 
 export default {
-  // FETCH_NEWS(context) {
-  //   fetchNewsList()
-  //     .then(response => {
-  //       context.commit('SET_NEWS', response.data);
-  //       return response.data; // Promise chaining을 하기 위해서
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // },
-  // FETCH_JOBS({ commit }) { // destructuring (ES6)
-  //   fetchJobsList()
-  //     .then(({ data }) => {
-  //       commit('SET_JOBS', data);
-  //       return data;
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // },
-  // FETCH_ASK({ commit }) {
-  //   fetchAskList()
-  //     .then(({ data }) => {
-  //       commit('SET_ASK', data);
-  //       return data;
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // },
-  FETCH_USER({ commit }, name) {
-    fetchUserInfo(name)
-      .then(({ data }) => {
-        commit('SET_USER', data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  async FETCH_USER({ commit }, name) {
+    const response = await fetchUserInfo(name);
+    commit('SET_USER', response.data);
+    return response;
   },
 
-  FETCH_ITEM({ commit }, id) {
-    fetchCommentItem(id)
-      .then(({ data }) => {
-        commit('SET_ITEM', data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  async FETCH_ITEM({ commit }, id) {
+    const response = await fetchCommentItem(id);
+    commit('SET_ITEM', response.data);
+    return response;
   },
-  FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName)
-      .then(({ data }) => {
-        commit('SET_LIST', data);
-        return data;
-      })
-      .catch(error => console.log(error));
+  async FETCH_LIST({ commit }, pageName) {
+    const response = await fetchList(pageName);
+    commit('SET_LIST', response.data);
+    return response;
   }
 }
