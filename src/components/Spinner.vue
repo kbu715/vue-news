7855c66052f483ad582d0a142c87343966714a46
kbu715 <kbu715@gmail.com>
2022-06-07 13:03:47 +0900

    feat: sass 추가

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -20,8 +20,8 @@ export default {
 };
 </script>
 
-<style scoped>
-.lds-facebook {
+<style lang="scss" scoped>
+/* .lds-facebook {
   display: inline-block;
   position: absolute;
   width: 64px;
@@ -53,6 +53,48 @@ export default {
   animation-delay: 0;
 }
 
+@keyframes lds-facebook {
+  0% {
+    top: 6px;
+    height: 51px;
+  }
+  50%,
+  100% {
+    top: 19px;
+    height: 26px;
+  }
+} */
+
+
+/* sass */
+.lds-facebook {
+  display: inline-block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  top: 47%;
+  left: 47%;
+  div {
+    display: inline-block;
+    position: absolute;
+    left: 6px;
+    width: 13px;
+    background: #42b883;
+    animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+    &:nth-child(1) {
+      left: 6px;
+      animation-delay: -0.24s;
+    }
+    &:nth-child(2) {
+      left: 26px;
+      animation-delay: -0.12s;
+    }
+    &:nth-child(3) {
+      left: 45px;
+      animation-delay: 0;
+    }
+  }
+}
 @keyframes lds-facebook {
   0% {
     top: 6px;
